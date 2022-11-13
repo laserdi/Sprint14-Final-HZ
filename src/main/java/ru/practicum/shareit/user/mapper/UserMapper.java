f8a249c87f2210de1939b6032fe3732312e86630
laserdi <v013v@ya.ru>
2022-11-14 00:10:40 +0200

    ХЗ, чтобы сдать 14-й спринт. Finish. CheckStyle - Done.

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -5,12 +5,12 @@ import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 /**
- * Mapstruct хватит, чтобы на основании интерфейса UserMapper на этапе компиляции сгенерировать нужную реализацию,
- * которая будет переводить.
+ * Mapstruct хватит, чтобы на основании интерфейса UserMapper на этапе компиляции
+ * сгенерировать нужную реализацию, которая будет переводить.
  */
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User mapToModel(UserDto itemDto);
-    UserDto mapToDto(User user);
 
+    UserDto mapToDto(User user);
 }
